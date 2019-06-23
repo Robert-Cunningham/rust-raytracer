0484836c41f7f134293eca98e3292d9df6c630c2
Robert Cunningham <robertvcunningham@gmail.com>
2019-06-23 15:34:33 -0400

    Add vec3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::ops;
+use std::fmt;
 
+mod vec3;
 
 fn main() {
     let x: &str = "Hello World!";
     println!("{}", x);
-    save_image();
+    save_image(20, 10);
+    println!("{}", vec3::vec3{x: 1.0, y: 2.0, z: 4.0} * vec3::vec3{x: 1.0, y: 4.0, z: 3.0})
 }
 
-fn save_image() -> std::io::Result<()> {
-    let mut file = File::create("foo.txt")?;
-    file.write_all(b"This is a test")?;
+fn save_image(x : i32, y : i32) -> std::io::Result<()> {
+    let mut file = File::create("foo.ppm")?;
+    file.write_all(b"P3\n")?;
+    file.write_all(format!("{} {}\n", x, y).as_bytes())?;
+    file.write_all(b"255\n")?;
+
+    for j in 0..y {
+        for i in 0..x {
+            let r = 255 * i / x as i32;
+            let g = 255 * j / y as i32;
+            let b = 255 * 0.5 as i32;
+            file.write_all(format!("{} {} {} \n", r, g, b).as_bytes())?;
+        }
+        //file.write_all(b"\n");
+    }
     Ok(())
 }
---
+++ src/vec3.rs
@@ -0,0 +1,61 @@
+use std::ops;
+use std::fmt;
+
+pub struct vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl vec3 {
+    fn length(&self) -> f64 {
+        return (self.x * self.x + self.y * self.y + self.z * self.z).powf(0.5);
+    }
+}
+
+impl fmt::Display for vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return write!(f, "{} {} {}", self.x, self.y, self.z);
+    }
+}
+
+impl ops::Add<vec3> for vec3 {
+
+    type Output = vec3;
+
+    fn add(self, r: vec3) -> vec3 {
+        return vec3{x: r.x + self.x, y: r.y + self.y, z: r.z + self.z}
+    }
+}
+
+impl ops::Div<vec3> for vec3 {
+
+    type Output = vec3;
+
+    fn div(self, r: vec3) -> vec3 {
+        return vec3{x: self.x / r.x, y: self.y / r.y, z: self.z / r.z}
+    }
+}
+
+impl vec3 {
+    fn dot(self, r: vec3) -> f64 {
+        return self.x * r.x + self.y * r.y + self.z * r.z
+    }
+}
+
+impl ops::Sub<vec3> for vec3 {
+
+    type Output = vec3;
+
+    fn sub(self, r: vec3) -> vec3 {
+        return vec3{x: self.x - r.x, y: self.y - r.y, z: self.z - r.z}
+    }
+}
+
+impl ops::Mul<vec3> for vec3 {
+    type Output = vec3;
+
+    fn mul(self, r: vec3) -> vec3 {
+        return vec3{x: self.x * r.x, y: self.y * r.y, z: self.z * r.z}
+    }
+}
